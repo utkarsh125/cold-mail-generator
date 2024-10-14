f4f536748ac734b79d7b3dd963592c2de8ed14fd
Utkarsh Pandey <utk2665@gmail.com>
2024-10-14 19:43:59 +0530

    Webpage complete

--- app/chains.py
+++ app/chains.py
@@ -1,25 +1,15 @@
 import os
-from langchain_core import ChatGroq
+from langchain_groq import ChatGroq
 from langchain_core.prompts import PromptTemplate
-from langchain_core.output_parsers import JsonOutputKeyToolsParser
+from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.exceptions import OutputParserException
 from dotenv import load_dotenv
 
 load_dotenv()
 
-os.getenv("GROQ_API_KEY")
-
 class Chain:
     def __init__(self):
-        self.llm = ChatGroq(
-            model="llama-3.1-70b-versatile",
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-            groq_api_key="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"
-            # other params...
-        )
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
 
     def extract_jobs(self, cleaned_text):
         prompt_extract = PromptTemplate.from_template(
@@ -28,7 +18,7 @@ class Chain:
             {page_data}
             ### INSTRUCTION:
             The scraped text is from the career's page of a website.
-            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills`, and `description`.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
             Only return the valid JSON.
             ### VALID JSON (NO PREAMBLE):
             """
@@ -49,22 +39,22 @@ class Chain:
             {job_description}
 
             ### INSTRUCTION:
-            You are Utkarsh, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
+            You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
             Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
             in fulfilling their needs.
             Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
-            Remember you are Utkarsh, BDE at AtliQ. 
+            Remember you are Mohan, BDE at AtliQ. 
             Do not provide a preamble.
             ### EMAIL (NO PREAMBLE):
+
             """
         )
         chain_email = prompt_email | self.llm
         res = chain_email.invoke({"job_description": str(job), "link_list": links})
         return res.content
 
-
-if __name == '__main__':
+if __name__ == "__main__":
     print(os.getenv("GROQ_API_KEY"))
